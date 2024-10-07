91caa097d4361600a45caee30093ee228d4ed72d
오우주 <oasispapa@gmail.com>
2024-10-07 10:47:53 +0900

    사번을 리턴하는 컨트롤러

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,18 +1,16 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
 
 	@GetMapping
-	public String getUserFromPathString(@PathVariable String userId) {
+	public String getUserFromPathString() {
 		return "82265630";
 	}
 
